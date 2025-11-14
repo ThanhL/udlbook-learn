9fa4b3d6990c65280ed8c3f9a542e37b3fee3959
unknown <thanhl354@gmail.com>
2025-11-14 18:00:20 +1100

    Added hydra configs

--- MLOps_Tute/diffusion_mnist/train.py
+++ MLOps_Tute/diffusion_mnist/train.py
@@ -1,6 +1,8 @@
 import os
-from scipy import datasets
+
+import hydra
 import torch
+import logging
 import torch.utils.data as data
 from torchvision import transforms, datasets
 
@@ -9,10 +11,20 @@ from lightning.pytorch.callbacks import ModelCheckpoint
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.loggers import TensorBoardLogger, WandbLogger
 
+from omegaconf.omegaconf import OmegaConf
 from model import UNetDiffSystem
 
 
-def main():
+# -- Main
+@hydra.main(version_base=None,
+            config_path="./configs",
+            config_name="config")
+def main(cfg):
+    OmegaConf.resolve(cfg)
+
+    logger = logging.getLogger(__name__)
+    logger.info(OmegaConf.to_yaml(cfg, resolve=True))
+
     # -- Data setup
     transform = transforms.ToTensor()
     train_set = datasets.MNIST(root=os.getcwd(),
@@ -25,25 +37,38 @@ def main():
                               transform=transform)
 
     # use 20% of training data for validation
-    train_set_size = int(len(train_set) * 0.8)
+    train_set_size = int(len(train_set) * cfg.training.train_val_split)
     valid_set_size = len(train_set) - train_set_size
 
     # split the train set into two
-    seed = torch.Generator().manual_seed(42)
+    seed = torch.Generator().manual_seed(cfg.training.seed)
     train_set, valid_set = data.random_split(train_set,
                                              [train_set_size, valid_set_size],
                                              generator=seed)
 
-    train_loader = torch.utils.data.DataLoader(train_set,
-                                               batch_size=256,
-                                               shuffle=True)
-    valid_loader = torch.utils.data.DataLoader(valid_set,
-                                               batch_size=256,
-                                               shuffle=False)
+    train_loader = torch.utils.data.DataLoader(
+        train_set,
+        batch_size=cfg.train_dataloader.batch_size,
+        shuffle=cfg.train_dataloader.shuffle,
+        num_workers=cfg.train_dataloader.num_workers,
+        # pin_memory=cfg.train_dataloader.pin_memory,
+        # persistent_workers=cfg.train_dataloader.persistent_workers,
+    )
+    valid_loader = torch.utils.data.DataLoader(
+        valid_set,
+        batch_size=cfg.val_dataloader.batch_size,
+        num_workers=cfg.val_dataloader.num_workers,
+        shuffle=cfg.val_dataloader.shuffle,
+        # pin_memory=cfg.val_dataloader.pin_memory,
+        # persistent_workers=cfg.val_dataloader.persistent_workers,
+    )
 
     # -- Model setup
     # Model
-    model = UNetDiffSystem(num_diffusion_iters=100, learning_rate=1e-4)
+    model = UNetDiffSystem(
+        num_diffusion_iters=cfg.diffusion.noise_scheduler.num_train_timesteps,
+        learning_rate=cfg.optimizer.lr
+    )
 
     # Checkpoints
     checkpoint_callback = ModelCheckpoint(
@@ -65,12 +90,12 @@ def main():
     trainer = L.Trainer(
         default_root_dir="logs",
         devices=(1 if torch.cuda.is_available() else 0),
-        max_epochs=10,
+        max_epochs=cfg.training.max_epochs,
         fast_dev_run=False,
         logger=logger,
         callbacks=[
             checkpoint_callback,
-            # early_stopping_callback,
+            early_stopping_callback,
         ],
     )
     trainer.fit(model=model,
